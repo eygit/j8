d1afd6d8768e56b01db0ed1b8f860294f9efe755
eiichiro.yoshida <eiichiro.yoshida@gmail.com>
2014-08-20 01:01:41 +0900

    Add 01_01 and Update 01_04

--- 01_04/src/RambdaSort.java
+++ 01_04/src/RambdaSort.java
@@ -1,12 +1,25 @@
 import java.io.File;
+import java.util.Arrays;
 
 
 public class RambdaSort {
 
+	private static void sortAndPrint(File[] files) {
+		Arrays.sort(files, (first, second)
+					-> first.toString().compareTo(second.toString())
+				);
+		
+		for (File file : files)
+			System.out.println(file.toString());
+	}
+	
 	public static void main(String[] args) {
 		File root = new File("C:\\");
 		File[] dirs = root.listFiles(File::isDirectory);
 		File[] files = root.listFiles(File::isFile);
+		
+		sortAndPrint(dirs);
+		sortAndPrint(files);
 
 	}
 
