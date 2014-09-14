db1cf7748f52ea69f038d2b573c70a543d7eab11
eiichiro.yoshida <eiichiro.yoshida@gmail.com>
2014-09-15 02:56:39 +0900

    Add 02_03

---
+++ 02_03/src/ParallelForLoop.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class ParallelForLoop {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		
+        String contents = new String(Files.readAllBytes(
+                Paths.get("./alice.txt")), StandardCharsets.UTF_8);
+        List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
+
+        long count = 0;
+        long before = 0;
+        before = System.nanoTime();
+        count = words.stream().filter(w -> w.length() > 12).count();
+        System.out.println("alice.txt: normal:\t" + (System.nanoTime() - before) + "\tcount:" + count);
+        before = System.nanoTime();
+        count = words.parallelStream().filter(w -> w.length() > 12).count();
+        System.out.println("alice.txt: parallel:\t" + (System.nanoTime() - before) + "\tcount:" + count);
+
+        
+        contents = new String(Files.readAllBytes(
+                Paths.get("./war-and-peace.txt")), StandardCharsets.UTF_8);
+        words = Arrays.asList(contents.split("[\\P{L}]+"));
+        before = System.nanoTime();
+        count = words.stream().filter(w -> w.length() > 12).count();
+        System.out.println("war-and-peace.txt: normal:\t" + (System.nanoTime() - before) + "\tcount:" + count);
+        before = System.nanoTime();
+        count = words.parallelStream().filter(w -> w.length() > 12).count();
+        System.out.println("war-and-peace.txt: parallel:\t" + (System.nanoTime() - before) + "\tcount:" + count);
+
+	}
+}
