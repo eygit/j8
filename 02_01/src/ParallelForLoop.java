0083cc3dd6e7ef26d3d1b237dca8f56b60d8e7c7
eyoshida <eiichiro.yoshida@nts.ricoh.co.jp>
2014-09-25 19:28:12 +0900

    Update answer.

--- 02_01/src/ParallelForLoop.java
+++ 02_01/src/ParallelForLoop.java
@@ -16,55 +16,27 @@ public class ParallelForLoop {
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
         AtomicInteger atomicCounter = new AtomicInteger();
-        AtomicInteger endFlag = new AtomicInteger();
-        AtomicInteger startSeg = new AtomicInteger();
+        AtomicInteger endSegment = new AtomicInteger();
+        AtomicInteger segNoCounter = new AtomicInteger();
 
         int segs = 4;
         int segSize = words.size() / segs;
 
         for (int seg = 0; seg < segs; seg++) {
             new Thread( () -> {
-            	int segNo = startSeg.getAndIncrement();
-            	for (int i = segSize*segNo; i < segSize*(segNo+1); i++) {
+            	int mySegNo = segNoCounter.getAndIncrement();
+            	for (int i = segSize*mySegNo; i < segSize*(mySegNo+1); i++) {
             		if (words.get(i).length() > 12) atomicCounter.addAndGet(1);
             	}
-            	endFlag.addAndGet(1);
+            	endSegment.addAndGet(1);
             }).start();
         }
 
 
-
-//        int start = 0;
-//        new Thread( () -> {
-//        	for (int i = start*0; i < start+size*1; i++) {
-//        		if (words.get(i).length() > 12) atomicCounter.addAndGet(1);
-//        	}
-//        	endFlag.addAndGet(1);
-//        }).start();
-//
-//        new Thread( () -> {
-//        	for (int i = start+size*1; i < start+size*2; i++) {
-//        		if (words.get(i).length() > 12) atomicCounter.addAndGet(1);
-//        	}
-//        	endFlag.addAndGet(1);
-//        }).start();
-//        new Thread( () -> {
-//        	for (int i = start+size*2; i < start+size*3; i++) {
-//        		if (words.get(i).length() > 12) atomicCounter.addAndGet(1);
-//        	}
-//        	endFlag.addAndGet(1);
-//        }).start();
-//        new Thread( () -> {
-//        	for (int i = start+size*3; i < start+size*4; i++) {
-//        		if (words.get(i).length() > 12) atomicCounter.addAndGet(1);
-//        	}
-//        	endFlag.addAndGet(1);
-//        }).start();
-
         //処理の待ち合わせが必要になるため単一カウンター更新にスレッドは使いたくない
         while(true) {
         	Thread.sleep(1000);
-        	if (endFlag.get() == 4) break;
+        	if (endSegment.get() == segs) break;
         }
         System.out.println(atomicCounter.get());
 
