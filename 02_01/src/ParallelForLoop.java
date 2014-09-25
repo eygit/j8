ef0a9c69d65c2b6118a33e2d5128ca54037361c3
eyoshida <eiichiro.yoshida@nts.ricoh.co.jp>
2014-09-25 19:02:53 +0900

    Update answer.

--- 02_01/src/ParallelForLoop.java
+++ 02_01/src/ParallelForLoop.java
@@ -10,52 +10,57 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class ParallelForLoop {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
-		
+
         String contents = new String(Files.readAllBytes(
                 Paths.get("./alice.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-//        long count = 0;
-//        for (String w : words) {
-//           if (w.length() > 12) count++;  
-//        }
-//        System.out.println(count);
-        // answer 33
-        
-        
         AtomicInteger atomicCounter = new AtomicInteger();
         AtomicInteger endFlag = new AtomicInteger();
-       
-        int segs = 4;
-        int size = words.size() / segs;
+        AtomicInteger startSeg = new AtomicInteger();
 
-        int start = 0;
-        new Thread( () -> {
-        	for (int i = start*0; i < start+size*1; i++) {
-        		if (words.get(i).length() > 12) atomicCounter.addAndGet(1);
-        	}
-        	endFlag.addAndGet(1);
-        }).start();
-        
-        new Thread( () -> {
-        	for (int i = start+size*1; i < start+size*2; i++) {
-        		if (words.get(i).length() > 12) atomicCounter.addAndGet(1);
-        	}
-        	endFlag.addAndGet(1);
-        }).start();
-        new Thread( () -> {
-        	for (int i = start+size*2; i < start+size*3; i++) {
-        		if (words.get(i).length() > 12) atomicCounter.addAndGet(1);
-        	}
-        	endFlag.addAndGet(1);
-        }).start();
-        new Thread( () -> {
-        	for (int i = start+size*3; i < start+size*4; i++) {
-        		if (words.get(i).length() > 12) atomicCounter.addAndGet(1);
-        	}
-        	endFlag.addAndGet(1);
-        }).start();
-        
+        int segs = 4;
+        int segSize = words.size() / segs;
+
+        for (int seg = 0; seg < segs; seg++) {
+            new Thread( () -> {
+            	int segNo = startSeg.getAndIncrement();
+            	for (int i = segSize*segNo; i < segSize*(segNo+1); i++) {
+            		if (words.get(i).length() > 12) atomicCounter.addAndGet(1);
+            	}
+            	endFlag.addAndGet(1);
+            }).start();
+        }
+
+
+
+//        int start = 0;
+//        new Thread( () -> {
+//        	for (int i = start*0; i < start+size*1; i++) {
+//        		if (words.get(i).length() > 12) atomicCounter.addAndGet(1);
+//        	}
+//        	endFlag.addAndGet(1);
+//        }).start();
+//
+//        new Thread( () -> {
+//        	for (int i = start+size*1; i < start+size*2; i++) {
+//        		if (words.get(i).length() > 12) atomicCounter.addAndGet(1);
+//        	}
+//        	endFlag.addAndGet(1);
+//        }).start();
+//        new Thread( () -> {
+//        	for (int i = start+size*2; i < start+size*3; i++) {
+//        		if (words.get(i).length() > 12) atomicCounter.addAndGet(1);
+//        	}
+//        	endFlag.addAndGet(1);
+//        }).start();
+//        new Thread( () -> {
+//        	for (int i = start+size*3; i < start+size*4; i++) {
+//        		if (words.get(i).length() > 12) atomicCounter.addAndGet(1);
+//        	}
+//        	endFlag.addAndGet(1);
+//        }).start();
+
         //処理の待ち合わせが必要になるため単一カウンター更新にスレッドは使いたくない
         while(true) {
         	Thread.sleep(1000);
