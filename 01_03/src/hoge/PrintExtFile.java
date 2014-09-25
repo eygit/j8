d6d3d901ea6e98e25a54c93e7c22e6f5756778f5
eyoshida <eiichiro.yoshida@nts.ricoh.co.jp>
2014-09-25 18:42:39 +0900

    FIX answer.

--- 01_03/src/hoge/PrintExtFile.java
+++ 01_03/src/hoge/PrintExtFile.java
@@ -4,21 +4,20 @@ import java.io.File;
 
 public class PrintExtFile {
 
+	// extnameがキャプチャされる変数
 	private static void printExtFile(String pathname, String extname) {
 		File root = new File(pathname);
-		File[] files = root.listFiles( (dir, name) ->
-					{
-						String strings[] = name.split("\\.");
-						return extname.equals(strings[strings.length - 1]);
-					}
-				);
-		for (File file : files)
-			System.out.println(file);
+		String[] filenames = root.list( (dir, name) -> {
+			String strings[] = name.split("\\.");
+			return extname.equals(strings[strings.length - 1]);
+		});
+		for (String filename : filenames)
+			System.out.println(filename);
 	}
 
 	public static void main(String[] args) {
-		String pathname = "C:\\"; // キャプチャされる変数
-		String extname = "txt"; // キャプチャされる変数
+		String pathname = "C:\\";
+		String extname = "log";
 		printExtFile(pathname, extname);
 
 
