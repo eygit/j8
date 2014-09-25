0822703468aee71b029c0b09eb8599c3c5f5c910
eyoshida <eiichiro.yoshida@nts.ricoh.co.jp>
2014-09-25 18:43:28 +0900

    FIX answer.

--- 01_04/src/RambdaSort.java
+++ 01_04/src/RambdaSort.java
@@ -5,22 +5,31 @@ import java.util.Arrays;
 public class RambdaSort {
 
 	private static void sortAndPrint(File[] files) {
-		Arrays.sort(files, (first, second)
-					-> first.toString().compareTo(second.toString())
-				);
-		
+		Arrays.sort(files, (first, second) -> {
+			boolean fd = first.isDirectory();
+			boolean sd = second.isDirectory();
+			if (fd) {
+				if (sd) {
+					return first.toString().compareTo(second.toString());
+				} else {
+					return -1;
+				}
+			} else {
+				if (sd) {
+					return 1;
+				} else {
+					return first.toString().compareTo(second.toString());
+				}
+			}
+		});
+
 		for (File file : files)
 			System.out.println(file.toString());
 	}
-	
+
 	public static void main(String[] args) {
 		File root = new File("C:\\");
-		File[] dirs = root.listFiles(File::isDirectory);
-		File[] files = root.listFiles(File::isFile);
-		
-		sortAndPrint(dirs);
-		sortAndPrint(files);
-
+		sortAndPrint(root.listFiles());
 	}
 
 }
