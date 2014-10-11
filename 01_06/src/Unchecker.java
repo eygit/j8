2098f6233018dcf89bfb0dd6ec3fe6df542a0413
eiichiro.yoshida <eiichiro.yoshida@gmail.com>
2014-10-11 12:58:17 +0900

    Update comment

--- 01_06/src/Unchecker.java
+++ 01_06/src/Unchecker.java
@@ -1,6 +1,8 @@
 
 public class Unchecker {
 
+	// RunnableExでCallback<Void>を定義してもThreadの引数の型(Runnable)とは異なるため
+	// 型推定で不整合を生じ、利用できない
 	public static Runnable uncheck(RunnableEx runner) {
 		return  () -> {
 			try {
