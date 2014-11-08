86d4bff82d9b32b46f2afdbf4088748fff737f4a
eiichiro.yoshida <eiichiro.yoshida@gmail.com>
2014-11-09 03:09:07 +0900

    Update answer.

--- 03_17/src/ExceptionAsync.java
+++ 03_17/src/ExceptionAsync.java
@@ -1,5 +1,6 @@
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
@@ -7,10 +8,12 @@ public class ExceptionAsync {
    public static void doInParallelOrder(Runnable first, Runnable second, Consumer<Throwable> handler) {
 	   try {
 		   ExecutorService pool = Executors.newCachedThreadPool();
-		   pool.submit(() -> first.run());
-		   pool.submit(() -> second.run());
+		   Future<?> ff = pool.submit(() -> first.run());
+		   Future<?> fs = pool.submit(() -> second.run());
 	       pool.shutdown();
 	       pool.awaitTermination(1, TimeUnit.HOURS);
+	       ff.get();
+	       fs.get();
 	   } catch (Throwable ex) {
 		   handler.accept(ex);
 	   }
@@ -18,16 +21,27 @@ public class ExceptionAsync {
 
    public static void main(String[] args) {
 	   //例外なし
-//      doInParallelOrder(
-//         () -> System.out.println("first: " + Thread.currentThread()),
-//         () -> System.out.println("second: " + Thread.currentThread()),
-//         (e) -> System.out.println("exception: "  + Thread.currentThread() + ":: " + e));
-      
-      // firstが例外スロー キャッチできない・・・。要件等
       doInParallelOrder(
-    	         () -> {throw new RuntimeException("fist exception");},
-    	         () -> System.out.println("second: " + Thread.currentThread()),
-    	         (e) -> System.out.println("exception: "  + Thread.currentThread() + ":: " + e));      
+         () -> System.out.println("first: " + Thread.currentThread()),
+         () -> System.out.println("second: " + Thread.currentThread()),
+         (e) -> System.out.println("hndle exception: "  + Thread.currentThread() + ":: " + e));
+      
+      System.out.println("=============");
+      // firstが例外スロー
+      doInParallelOrder(
+			() -> {throw new RuntimeException("fist exception");},
+			() -> System.out.println("second: " + Thread.currentThread()),
+			(e) -> System.out.println("hndle exception: "  + Thread.currentThread() + ":: " + e));      
+      System.out.println("=============");
+      
+      // secondが例外スロー
+      doInParallelOrder(
+			() -> System.out.println("fist: " + Thread.currentThread()),
+			() -> {throw new RuntimeException("second exception");},
+			(e) -> System.out.println("hndle exception: "  + Thread.currentThread() + ":: " + e));  
 
+      // fist, second両方が例外のときは、今回の実装では、fistのみ出力となる
+      
+ 
    }
 }
