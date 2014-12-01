d0e3c8bc63447e9ab83a2dcf59d634d6fb2a9bfa
eyoshida <eiichiro.yoshida@nts.ricoh.co.jp>
2014-12-01 19:47:20 +0900

    Add a answer.

--- 05_03/src/NextWeekDay.java
+++ 05_03/src/NextWeekDay.java
@@ -1,4 +1,5 @@
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAdjuster;
 import java.util.function.Predicate;
 
@@ -7,19 +8,25 @@ public class NextWeekDay {
 
 	static TemporalAdjuster next(Predicate<LocalDate> date) {
 		// 関数型インタフェース：　Temporal adjustInto(Temporal temporal)　の実装.
-		return null;
-//		return t -> {
-//			Temporal candidate = t.plus(1, ChronoUnit.DAYS);
-//			while (date) {
-//
-//			}
-//			return t.plus(1, ChronoUnit.DAYS);
-//		};
+		return t -> {
+			LocalDate candidate = LocalDate.from(t).plus(1, ChronoUnit.DAYS);
+			while (! date.test((LocalDate) candidate)) {
+				candidate = candidate.plus(1, ChronoUnit.DAYS);
+			}
+			return candidate;
+		};
 	}
 
 	public static void main(String[] args) {
-		LocalDate today = LocalDate.of(2014, 12, 01);
+		LocalDate today = LocalDate.of(2014, 12, 01); // 月曜日
 		LocalDate nextWeekDay = today.with(next( w ->
+					w.getDayOfWeek().getValue() < 6
+				));
+		System.out.println(nextWeekDay);
+
+
+		today = LocalDate.of(2014, 12, 05); // 金曜日
+		nextWeekDay = today.with(next( w ->
 					w.getDayOfWeek().getValue() < 6
 				));
 		System.out.println(nextWeekDay);
