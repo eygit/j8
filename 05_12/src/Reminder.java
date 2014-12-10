c0e7be178c254c0eb6b62788b15dea0299c08543
eyoshida <eiichiro.yoshida@nts.ricoh.co.jp>
2014-12-10 17:32:50 +0900

    Add temp.

---
+++ 05_12/src/Reminder.java
@@ -0,0 +1,12 @@
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+
+public class Reminder {
+
+	public static void main(String[] args) {
+		ZonedDateTime task1 = ZonedDateTime.of(2014, 12, 10, 12, 0, 0, 0, ZoneId.of("CET"));
+
+	}
+
+}
