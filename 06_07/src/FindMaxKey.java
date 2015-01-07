87eec733b4c20f617c71b503b1db6f799df5c9ae
eyoshida <eiichiro.yoshida@nts.ricoh.co.jp>
2015-01-07 11:46:45 +0900

    Add a answer.

--- 06_07/src/FindMaxKey.java
+++ 06_07/src/FindMaxKey.java
@@ -1,11 +1,26 @@
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * ConcurrentHashMap<String, Long>内で、最大値を持つキーを見つけなさい
+ * (同じ最大値を持つキーがあれば、どちらのキーでもかまいません)。
+ * ヒント：reduceEntries
+ *
+ */
 
 public class FindMaxKey {
 
 	public static void main(String[] args) {
 		ConcurrentHashMap<String, Long> map = new ConcurrentHashMap<>();
-		//TODO: 続きをかく。
+		for (int i = 0; i < 10000; i++) {
+			map.put("key " + i, (long) i%365);
+		}
+
+		Map.Entry<String, Long> ans = map.reduceEntries(4, (t, u) -> {
+			return (t.getValue() > u.getValue()) ? t : u;
+		});
+
+		System.out.println(ans);
 
 	}
 
