6191a96606bb0fb05f59bea314e12a7300db1621
eyoshida <eyoshida@ubuntuvm.(none)>
2015-04-09 18:43:22 +0900

    add a answer.

--- 09_11/src/CreditShark.java
+++ 09_11/src/CreditShark.java
@@ -7,8 +7,8 @@ public class CreditShark {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 	      ProcessBuilder builder = new ProcessBuilder(
-	    	         "grep", "-o", "[A-Za-z_][A-Za-z_0-9]*");
-	    	      builder.redirectInput(Paths.get("CreditShark.java").toFile());
+	    	         "grep", "-o", "[0-9]*", "-r", "/home/<USERNAME>");
+	    	      /*builder.redirectInput(Paths.get("~/").toFile());*/
 	    	      builder.redirectOutput(Paths.get("identifiers.txt").toFile());
 	    	      Process process = builder.start();
 	    	      process.waitFor(1, TimeUnit.MINUTES);
