6079db1e85092ab633203e60804284e31d7f3749
eyoshida <eiichiro.yoshida@nts.ricoh.co.jp>
2015-04-30 17:13:58 +0900

    Add menu functions.

--- fxclock/src/FxClock.java
+++ fxclock/src/FxClock.java
@@ -1,6 +1,7 @@
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.util.Arrays;
 import java.util.List;
 
 import javafx.application.Application;
@@ -23,30 +24,11 @@ public class FxClock extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		BorderPane root = new BorderPane();
-		VBox topContainer = new VBox();
-		MenuBar mainMenu = new MenuBar();
-		Menu fontMenu = new Menu("Font");
 
-		List<String> fontFamilies = Font.getFamilies();
-		System.out.println(fontFamilies);
-		for (String fontFamily : fontFamilies) {
-			MenuItem fontFamilyItem = new MenuItem(fontFamily);
-			fontMenu.getItems().add(fontFamilyItem);
-		}
-
-
-		List<String> fontNames = Font.getFontNames();
-		System.out.println(fontNames);
-
-
-
-
-		Label message = new Label();
+		// Clock Label設定.
+		Label clockLabel = new Label();
 		DateTimeFormatter dateFormat = DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM);
-
 		ScheduledService<String> clockStringService = new ScheduledService<String>() {
-
 			@Override
 			protected Task<String> createTask() {
 				return new Task<String>() {
@@ -60,21 +42,52 @@ public class FxClock extends Application {
 			}
 		};
 		clockStringService.setOnScheduled( e -> {
-			message.setText(clockStringService.getLastValue());
+			clockLabel.setText(clockStringService.getLastValue());
 		});
 		clockStringService.setPeriod(Duration.seconds(1));
 		clockStringService.start();
+		clockLabel.setFont(new Font(30));
 
-		message.setFont(new Font(30));
+		// FontFamilyMenu設定.
+		List<String> fontFamilies = Font.getFamilies();
+		Menu fontFamilyMenu = new Menu("FontFamily");
+		for (String fontFamily : fontFamilies) {
+			MenuItem fontFamilyItem = new MenuItem(fontFamily);
+			fontFamilyItem.setOnAction(e -> clockLabel.setFont(new Font(fontFamily, clockLabel.getFont().getSize())));
+			fontFamilyMenu.getItems().add(fontFamilyItem);
+		}
 
 
+		// FontNameMenu設定.
+		List<String> fontNames = Font.getFontNames();
+		Menu fontNameMenu = new Menu("FontName");
+		for (String fontName : fontNames) {
+			MenuItem fontNameItem = new MenuItem(fontName);
+			fontNameItem.setOnAction(e -> clockLabel.setFont(new Font(fontName, clockLabel.getFont().getSize())));
+			fontNameMenu.getItems().add(fontNameItem);
+		}
 
-		mainMenu.getMenus().addAll(fontMenu);
-		topContainer.getChildren().addAll(mainMenu, message);
+		// FontSizeMenu設定.
+		List<Double> fontSiezes = Arrays.asList(8.0, 10.0, 12.0, 14.0, 18.0, 24.0, 32.0, 36.0, 48.0, 72.0);
+		Menu fontSizeMenu = new Menu("FontSize");
+		for (Double fontSize : fontSiezes) {
+			MenuItem fontSizeItem = new MenuItem(Integer.valueOf(fontSize.intValue()).toString());
+			fontSizeItem.setOnAction(e -> clockLabel.setFont(new Font(clockLabel.getFont().getName(), fontSize)));
+			fontSizeMenu.getItems().add(fontSizeItem);
+		}
+
+
+		// 画面組み立て
+		MenuBar mainMenu = new MenuBar();
+		mainMenu.getMenus().addAll(fontFamilyMenu, fontNameMenu, fontSizeMenu);
+		VBox topContainer = new VBox();
+		topContainer.getChildren().addAll(mainMenu, clockLabel);
+		BorderPane root = new BorderPane();
 		root.setTop(topContainer);
 
 		stage.setScene(new Scene(root));
-		stage.setWidth(200);
+		stage.setWidth(400);
+		stage.setHeight(150);
 		stage.setTitle("fx clock");
 		stage.show();
 
